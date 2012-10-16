6aaab3dd356519d7161aaa343dffe3aa36efbc9f
Johann <git@johann-hofmann.com>
2012-10-16 13:34:17 +0200

    !!SAFAI!! Objektorientierung / Kapselung vorgenommen.

--- src/a1/ImageViewer.java
+++ src/a1/ImageViewer.java
@@ -1,16 +1,59 @@
 package a1;
 
-import java.awt.Frame;
-import java.awt.FileDialog;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-public class ImageViewer {
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-	public static void main(String[] args) {
-		Frame myFrame = new Frame("Image Viewer");
-		FileDialog fd = new FileDialog(myFrame, "Select");
-		myFrame.setSize(500, 500);
-		myFrame.setVisible(true);
-		fd.setVisible(true);
+public class ImageViewer extends JComponent {
+
+	private static final long serialVersionUID = 1L;
+	protected BufferedImage img = null;
+	final static FileNameExtensionFilter FILTER = new FileNameExtensionFilter(
+			"Images", "jpg", "gif", "png");
+	protected JFileChooser fc;
+	protected final JFrame frame;
+
+	public ImageViewer(JFrame frame) {
+		this.frame = frame;
+		fc = new JFileChooser();
+		fc.setFileFilter(FILTER);
 	}
 
+	public void loadImage() {
+		if (fc.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+			try {
+				img = ImageIO.read(fc.getSelectedFile());
+			} catch (IOException e) {
+				loadImage();
+			}
+		}
+	}
+
+	public void paint(Graphics g) {
+		g.drawImage(img, 0, 0, null);
+	}
+	
+	private int getImageHeight() {
+		return img.getWidth();
+	}
+
+	private int getImageWidth() {
+		return img.getWidth();
+	}
+
+	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("Image Viewer");
+		ImageViewer viewer = new ImageViewer(myFrame);
+		viewer.loadImage();
+		myFrame.add(viewer);
+		myFrame.setSize(viewer.getImageWidth(), viewer.getImageHeight());
+		myFrame.setVisible(true);
+
+	}
 }
