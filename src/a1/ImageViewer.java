7949a391709e48e2f33cffb9c828da8f02729b71
Johann <git@johann-hofmann.com>
2012-10-29 18:30:22 +0100

    ImageViewer JavaDoc

--- src/a1/ImageViewer.java
+++ src/a1/ImageViewer.java
@@ -1,6 +1,7 @@
 package a1;
 
 import java.awt.Graphics;
+import java.awt.Window;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
@@ -11,13 +12,20 @@ import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
-import javax.swing.JOptionPane;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-//Subklasse von JComponent, damit sie relativ leicht überall eingebunden werden kann
-public class ImageViewer extends JComponent  {
+/**
+ * A Component that allows quick and simple embedding of user selected images.
+ * <p>
+ * After creating a new ImageViewer, you should load an image using {@link #loadImage()}
+ * 
+ */
+
+// Subklasse von JComponent, damit sie relativ leicht überall eingebunden werden kann
+public class ImageViewer extends JComponent {
 
 	private static final long serialVersionUID = 1L;
 
@@ -27,17 +35,31 @@ public class ImageViewer extends JComponent  {
 	// Dialog-Fenster zum auswählen einer Datei
 	protected final JFileChooser fileDialog;
 	// Filter für erlaubte Bildformate im fileDialog
-	protected final static FileNameExtensionFilter FILTER = new FileNameExtensionFilter("Images",
-			"jpg", "gif", "png", "bmp", "tif", "ami", "apx", "bmp", "brk", "bw", "cal", "cbm", "cbr","cbz", "cpt", "cur", "dds", "dng", "exr", "fif", "fpx");
+	protected final static FileNameExtensionFilter FILTER = new FileNameExtensionFilter("Images", "jpg", "gif", "png", "bmp", "tif");
 
 	// Variablen unseres Testprogramms //
-	protected final JFrame frame;
+	protected final Window frame;
 
-	public ImageViewer(final JFrame frame) {
+	/**
+	 * Creates an ImageViewer that belongs to the given {@link Window}.
+	 */
+
+	public ImageViewer(final Window frame) {
 		this.frame = frame;
 		fileDialog = new JFileChooser();
 		fileDialog.setFileFilter(FILTER);
 	}
+
+	/**
+	 * Causes the ImageViewer to show a {@link JFileChooser} and replace the image with the selected File.
+	 * <p>
+	 * If the File cannot be found, a {@link JOptionPane}-warning is shown.
+	 * <p>
+	 * A {@link FileNameExtensionFilter} is used to filter the selection of shown files in the dialog.
+	 * 
+	 * If the user cancels the first filedialog or manages to select a non-image file, the image will be empty.
+	 * 
+	 */
 
 	public void loadImage() {
 		if (fileDialog.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
@@ -45,14 +67,12 @@ public class ImageViewer extends JComponent  {
 			try {
 				img = ImageIO.read(fileDialog.getSelectedFile());
 			} catch (IOException e) {
-				// falls es einen Fehler beim Laden gibt, wird der Dialog einfach neu angezeigt
-				// //TODO: kann man das eleganter machen? Vielleicht vorher eine Fehlermeldung (Popup)?
+				// falls es einen Fehler beim Laden gibt, wird eine Fehlermeldung angezeigt
 				JOptionPane.showMessageDialog(frame, "Fehler bei Dateiauswahl");
 				loadImage();
 			}
 		} else {
-			// Wenn kein Bild ausgewählt wird
-			// // TODO: Was passiert wenn kein Bild ausgewählt wird?
+			// Wenn Cancel gedrückt wird passiert einfach nichts.
 		}
 	}
 
@@ -62,8 +82,15 @@ public class ImageViewer extends JComponent  {
 		g.drawImage(img, 0, 0, null);
 	}
 
+	/**
+	 * 
+	 * Returns the height of the {@link BufferedImage}. Returns 0 if it does not exist (yet).
+	 * 
+	 * @return the height of the image or 0 if the image does not exist.
+	 */
+
 	// liefert die Höhe unseres Bildes oder 0, wenn es nicht existiert
-	private int getImageHeight() {
+	public int getImageHeight() {
 		if (img == null) {
 			return 0;
 		} else {
@@ -71,8 +98,15 @@ public class ImageViewer extends JComponent  {
 		}
 	}
 
+	/**
+	 * 
+	 * Returns the width of the {@link BufferedImage}. Returns 0 if it does not exist (yet).
+	 * 
+	 * @return the width of the image or 0 if the image does not exist.
+	 */
+
 	// liefert die Breite unseres Bildes oder 0, wenn es nicht existiert
-	private int getImageWidth() {
+	public int getImageWidth() {
 		if (img == null) {
 			return 0;
 		} else {
@@ -86,55 +120,50 @@ public class ImageViewer extends JComponent  {
 
 		JMenuBar menubar = new JMenuBar();
 		myFrame.setJMenuBar(menubar);
-		
+
 		viewer.loadImage();
 		myFrame.add(viewer);
 
 		myFrame.setSize(viewer.getImageWidth(), viewer.getImageHeight());
 		myFrame.setVisible(true);
 
-		JMenu neuesBild = new JMenu("Neues Bild");
-		menubar.add(neuesBild);
-		JMenuItem bildLaden = new JMenuItem("Bild laden");
-		neuesBild.add(bildLaden);
-		
-	
-		bildLaden.addMouseListener(new MouseListener (){
+		JMenu newImageMenu = new JMenu("Neues Bild");
+		menubar.add(newImageMenu);
+		JMenuItem loadImageButton = new JMenuItem("Bild laden");
+		newImageMenu.add(loadImageButton);
+
+		loadImageButton.addMouseListener(new MouseListener() {
 
 			@Override
-			public void mouseClicked(MouseEvent e) { 
-				//wird nicht genutzt, aber wird benötigt um den MouseListener zu erstellen
-				
+			public void mouseClicked(MouseEvent e) {
+				// wird nicht genutzt, aber wird benötigt um den MouseListener zu erstellen
+
 			}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				viewer.loadImage();
-				myFrame.setSize(viewer.getImageWidth(), viewer.getImageHeight());
-				myFrame.repaint();
-				
+				// wird nicht genutzt, aber wird benötigt um den MouseListener zu erstellen
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
-				//wird nicht genutzt, aber wird benötigt um den MouseListener zu erstellen
-				
+				// wird nicht genutzt, aber wird benötigt um den MouseListener zu erstellen
+
 			}
 
 			@Override
 			public void mousePressed(MouseEvent e) {
-				//wird nicht genutzt, aber wird benötigt um den MouseListener zu erstellen
-				
+				// wird nicht genutzt, aber wird benötigt um den MouseListener zu erstellen
+
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
-					
-			}});
-		
+				viewer.loadImage();
+				myFrame.setSize(viewer.getImageWidth(), viewer.getImageHeight());
+				myFrame.repaint();
+			}
+		});
 
-	
-		
-		
 	}
 }
